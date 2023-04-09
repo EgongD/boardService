81a686c02e4702607191869f00a01c8bbd3f7f06
EgongD <dlghtpdk@gmail.com>
2023-04-09 18:45:48 +0900

    update basic CRUD

--- src/main/java/member/mapper/MemberMapper.java
+++ src/main/java/member/mapper/MemberMapper.java
@@ -2,9 +2,12 @@ package member.mapper;
 
 import member.dto.MemberDto;
 import member.entity.Member;
+import org.mapstruct.Mapper;
 
 import java.util.List;
 
+
+@Mapper(componentModel = "spring")
 public interface MemberMapper {
 
     Member memberPostToMember(MemberDto.Post post);
